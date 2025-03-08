1e6b1afc0921959e4b64cac7112d834a83289607
Lucianomin <minda.lucian@yahoo.com>
2025-03-08 21:53:07 +0200

    setup tailwind working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  content: [
+    "./dist/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+   
+    },
+  }
+ 
 
