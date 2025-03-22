cc1e4971da961a391e353c10c81918a27d8b5b32
Lucianomin <minda.lucian@yahoo.com>
2025-03-22 23:30:30 +0200

    rear_car_images

--- js/main.js
+++ js/main.js
@@ -22,13 +22,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const viewImages = {
     "front-view-button": [
+"./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_05percent.jpg",
+"./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_20percent.jpg",      
 "./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_35percent.jpg",
 "./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_50percent.jpg",
 "./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_80percent.jpg",
 "./images/front_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Front_1_sc1.jpg"
     ],
     "side-view-button": [
-
+"./images/side_car/Toyota_Prius_Prime_2019_Side_05percent_rear.jpg",
+"./images/side_car/Toyota_Prius_Prime_2019_Side_20percent_rear.jpg",
 "./images/side_car/Toyota_Prius_Prime_2019_Side_35percent_rear.jpg",
 "./images/side_car/Toyota_Prius_Prime_2019_Plus_Hatchback_Side_1_50percent.jpg",
 "./images/side_car/Toyota_Prius_Prime_2019_Side_80percent_rear.jpg",
@@ -36,6 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     ],
     "back-view-button": [
+"./images/back_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Rear_1_05percent.jpg",
+"./images/back_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Rear_1_20percent.jpg",
 "./images/back_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Rear_1_35percent.jpg",
 "./images/back_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Rear_1_50percent.jpg",
 "./images/back_car/Toyota_Prius_Prime_2019_Plus_Hatchback_45_Rear_1_80percent.jpg",
